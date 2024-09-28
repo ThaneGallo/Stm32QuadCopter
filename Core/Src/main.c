3860428d8cff832256d32f2d51c42e4ef96ae635
ThaneGallo <thanegallo@gmail.com>
2024-09-28 13:48:10 -0400

    edited old library and seprated accel and gyro setup

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -128,7 +128,13 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 56;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -138,12 +144,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV8;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
     Error_Handler();
   }
@@ -168,7 +174,7 @@ static void MX_SPI1_Init(void)
   hspi1.Instance = SPI1;
   hspi1.Init.Mode = SPI_MODE_SLAVE;
   hspi1.Init.Direction = SPI_DIRECTION_2LINES_RXONLY;
-  hspi1.Init.DataSize = SPI_DATASIZE_16BIT;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi1.Init.NSS = SPI_NSS_SOFT;
---
+++ Drivers/STM32F4xx_HAL_Driver/Inc/ICM20948.h
@@ -0,0 +1,126 @@
+/*
+ * ICM20948.h
+ *
+ *  Created on: Oct 26, 2018
+ *      Author: cory
+ */
+
+#ifndef ICM20948_H_
+#define ICM20948_H_
+
+uint16_t accel_data[3];
+uint16_t gyro_data[3];
+int16_t mag_data[3];
+
+#define SPI_BUS			(&hspi1) // ***
+#define UART_BUS		(&huart3) // ***
+
+#define USER_BANK_SEL	(0x7F)
+#define USER_BANK_0		(0x00)
+#define USER_BANK_1		(0x10)
+#define USER_BANK_2		(0x20)
+#define USER_BANK_3		(0x30)
+
+#define PWR_MGMT_1 		(0x06)
+#define PWR_MGMT_2		(0x07)
+#define GYRO_CONFIG_1	(0x01)
+
+
+#define CLK_BEST_AVAIL	(0x01)
+#define GYRO_LPF_17HZ 	(0x29)
+
+// Gyro setup
+// full scale
+#define GYRO_FS_250 0x00
+#define GYRO_FS_500 0x02
+#define GYRO_FS_1000 0x04
+#define GYRO_FS_2000 0x05
+
+// how many samples to avg
+#define GYRO_AVG_CFG_1 0x00
+#define GYRO_AVG_CFG_2 0x01
+#define GYRO_AVG_CFG_4 0x02
+#define GYRO_AVG_CFG_8 0x03
+#define GYRO_AVG_CFG_16 0x04
+#define GYRO_AVG_CFG_32 0x05
+#define GYRO_AVG_CFG_64 0x06
+#define GYRO_AVG_CFG_128 0x07
+
+// ACCEL setup
+// full scale
+#define ACCEL_FS_2G 0x00
+#define ACCEL_FS_4G 0x02
+#define ACCEL_FS_8G 0x04
+#define ACCEL_FS_16G 0x05
+
+// how many samples to avg
+#define ACCEL_AVG_CFG_4 0x00
+#define ACCEL_AVG_CFG_8 0x01
+#define ACCEL_AVG_CFG_16 0x02
+#define ACCEL_AVG_CFG_32 0x03
+
+
+struct GYRO;
+struct ACCEL;
+struct ICM_20948;
+
+struct GYRO
+{
+    uint16_t x;
+    uint16_t y;
+    uint16_t z;
+    uint16_t sensitivity;
+};
+
+struct ACCEL
+{
+    uint16_t x;
+    uint16_t y;
+    uint16_t z;
+    uint16_t sensitivity;
+};
+
+struct MAGN
+{
+    uint16_t x;
+    uint16_t y;
+    uint16_t z;
+    uint16_t sensitivity;
+};
+
+struct CONFG
+{
+    uint16_t x;
+    uint16_t y;
+    uint16_t z;
+};
+
+typedef struct ICM_20948
+{
+    struct CONFG config;
+    struct GYRO gyro;
+    struct ACCEL accel;
+    struct MAGN magn;
+
+} ICM_20948;
+
+void ICM_gyro_config(uint8_t FS_config, uint8_t Avg_config, uint8_t DLPF_config);
+void ICM_accel_config(uint8_t FS_config, uint8_t Avg_config, uint8_t DLPF_config);
+
+void ICM_PowerOn();
+uint8_t ICM_WHOAMI(void);
+void ICM_SelectBank(uint8_t bank);
+void ICM_ReadAccelGyro(struct ICM_20948 *IMU);
+void ICM_ReadMag(struct ICM_20948 *IMU);
+uint16_t ICM_Initialize(void);
+void ICM_SelectBank(uint8_t bank);
+void ICM_Disable_I2C(void);
+void ICM_CSHigh(void);
+void ICM_CSLow(void);
+void ICM_SetClock(uint8_t clk);
+void ICM_AccelGyroOff(void);
+void ICM_AccelGyroOn(void);
+void ICM_SetGyroRateLPF(uint8_t rate, uint8_t lpf);
+void ICM_SetGyroLPF(uint8_t lpf);
+
+#endif /* ICM20948_H_ */
